4c7630c14150332dd5609585c2bd5f4e02754322
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2023-02-08 14:31:19 -0600

    Update and rename Freesurfer_ROIs_fsonly.py to AAL116_ROIs.py

--- scripts/AAL116_ROIs.py
+++ scripts/AAL116_ROIs.py
@@ -1,14 +1,14 @@
-#Creates ROI masks fom Freesurfer parcellation (which has previous been transformed to DTI Space)
+#Creates ROI masks fom AAL116 parcellation (which has previous been transformed to DTI Space)
 
 import os
 import nipype.interfaces.fsl as fsl
 import csv
 
-parcellation_num = int(os.environ['parcellation_number'])
+parcellation_num = int(116)
 parcellation_labels_file = os.environ['parcellation_labels_file']
 
-Freesurfer_Regions_dict = {}
-Freesurfer_Regions_list=[]
+AAL116_Regions_dict = {}
+AAL116_Regions_list=[]
 
 with open('{}/{}'.format(os.environ['SCRIPTS_DIR'],parcellation_labels_file), 'r') as f:
 	for line in range(parcellation_num):
@@ -18,12 +18,12 @@ with open('{}/{}'.format(os.environ['SCRIPTS_DIR'],parcellation_labels_file), 'r
 		else:
 			current_line = current_line[:-1]
 			current_line = current_line.split()
-		Freesurfer_Regions_dict[current_line[1]]=int(current_line[0])
-		Freesurfer_Regions_list.append(current_line[1])
+		AAL116_Regions_dict[current_line[1]]=int(current_line[0])
+		AAL116_Regions_list.append(current_line[1])
 
 #create text file with all ROI masks
 with open('masks.txt', 'w') as f:
-	for roi in Freesurfer_Regions_list:
+	for roi in AAL116_Regions_list:
 		f.write('{}/{}.nii.gz\n'.format(os.environ['RESDIR'], roi))
 
 
@@ -32,21 +32,21 @@ ROI_volumes_csv=[]
 #create each ROI niftii file
 for index in range(parcellation_num):           # index goes 1:70
 	# print 'Region Number {}'.format(index+1)
-	x = Freesurfer_Regions_dict[Freesurfer_Regions_list[index]]
+	x = AAL116_Regions_dict[AAL116_Regions_list[index]]
 	get_ROI = fsl.maths.Threshold()
-	get_ROI.inputs.in_file = 'FS_to_DTI.nii.gz'
+	get_ROI.inputs.in_file = 'aal116_atlas_fslstd.nii.gz'
 	get_ROI.inputs.thresh = x-0.5
 	get_ROI.inputs.args = '-uthr {}'.format(x+0.5)
-	get_ROI.inputs.out_file = '{}.nii.gz'.format(Freesurfer_Regions_list[index])
+	get_ROI.inputs.out_file = '{}.nii.gz'.format(AAL116_Regions_list[index])
 	get_ROI.run()
 
 	#get volume
-	current_ROI_file='{}.nii.gz'.format(Freesurfer_Regions_list[index])
+	current_ROI_file='{}.nii.gz'.format(AAL116_Regions_list[index])
 
 	get_volume_ROI = fsl.ImageStats(in_file=current_ROI_file, op_string='-V > ROI_volumes.txt')
 	get_volume_ROI.run()
 
-	roi_volumes_line=[Freesurfer_Regions_list[index]]
+	roi_volumes_line=[AAL116_Regions_list[index]]
 	with open('ROI_volumes.txt', 'r') as f:
 		lines=f.readlines()
 		line=lines[0].split()
