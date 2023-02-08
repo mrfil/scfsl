d1893fd0f4241ba50dbb8e83b655b7813d7c84ce
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2023-02-08 15:02:30 -0600

    Update CSF_mask.sh

--- scripts/CSF_mask.sh
+++ scripts/CSF_mask.sh
@@ -3,4 +3,4 @@
 #create CSF mask for tractography from probseg from QSIPrep
 
 fslreorient2std CSF_probseg.nii.gz CSF_probseg_fslstd.nii.gz
-fslmaths CSF_probseg.nii.gz -thr 0.5 -uthr 1 CSFmask.nii.gz
+fslmaths CSF_probseg_fslstd.nii.gz -thr 0.5 -uthr 1 CSFmask.nii.gz
